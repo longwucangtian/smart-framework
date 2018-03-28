3cc21be9373e2ca53f9ef70798c2a07e5e043eff
longwucangtian <920304907@qq.com>
2018-03-28 23:06:03 +0800

    modified

---
+++ src/main/java/org/smart4j/framework/util/CodecUtil.java
@@ -0,0 +1,47 @@
+package org.smart4j.framework.util;
+
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 编码与解码操作类工具
+ * @author lz
+ *
+ */
+public final class CodecUtil {
+	private static final Logger LOGGER = LoggerFactory.getLogger(CodecUtil.class);
+
+	/**
+	 * 将URL编码
+	 * @param source
+	 * @return
+	 */
+	public static String encodeURL(String source){
+		String target;
+		try{
+			target = URLEncoder.encode(source,"UTF-8");
+		}catch(Exception e){
+			LOGGER.error("encode URL failure",e);
+			throw new RuntimeException(e);
+		}
+		return target;
+	}
+	
+	/**
+	 * 将URL解码
+	 */
+	public static String decodeURL(String source){
+		String target;
+		try{
+			target = URLDecoder.decode(source,"UTF-8");
+		}catch(Exception e){
+			LOGGER.error("decode URL failure",e);
+			throw new RuntimeException(e);
+		}
+		return target;
+	}
+	
+}
